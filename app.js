28d58f785b95b5e042e2216644d0035e994cc119
bklee2 <zenossuper@gmail.com>
2020-02-24 11:54:53 +0900

    선 그리기 완성

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext("2d");
 
-// onmouseup
-// onmousedown
-// onmouseenter
-// onmouseleaveee
+let painting = false;
+
 function onCanvasMouseMove(e) {
     const x = e.offsetX;
     const y = e.offsetY;
 
-    console.dir(e);
-    // console.log(x, y);
+    //console.dir(e);
+    //console.log(x, y);
+
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 function init() {
-    canvas.addEventListener('mousemove', onCanvasMouseMove);
+    if(canvas) {
+        canvas.addEventListener('mousemove', onCanvasMouseMove);
+        canvas.addEventListener('mousedown', startPainting);
+        canvas.addEventListener('mouseup', stopPainting);
+        canvas.addEventListener('mouseleave', stopPainting);
+
+        canvas.width = 600;
+        canvas.height = 600;
+    }
+    
+    
+    ctx.storkeStyle = "#2c2c2c";
+    ctx.lineWith = "2.5";
 }
 
 init();
